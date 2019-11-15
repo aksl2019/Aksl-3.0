082511a514e321564f59dc7c52a5c998655bb05f
zheng ming <zheng1748@outlook.com>
2019-11-15 20:31:02 +0800

    core 3.0
    core 3.0

---
+++ Aksl.BulkInsert/Aksl.BulkInsert/BulkInsert/IBulkInserter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aksl.BulkInsert
+{
+    #region Dataflow IBulkInserter
+    /// <summary>
+    /// Bulk Inserter Interface
+    /// </summary>
+    public interface IDataflowBulkInserter<TMessage, TResult>
+    {
+        #region Properties
+        Action<BulkInsertContextContext<TResult>> OnInsertCallBack { get; set; }
+
+        Func<IEnumerable<TMessage>, Task<IEnumerable<TResult>>> InsertHandler { get; set; }
+        #endregion
+
+        #region Bulk Insert Method
+        Task<IEnumerable<TResult>> BulkInsertAsync(IEnumerable<TMessage> messages, CancellationToken cancellationToken = default);
+        #endregion
+    }
+    #endregion
+
+    #region DataflowPipe IBulkInserter
+    /// <summary>
+    /// Bulk Inserter Interface
+    /// </summary>
+    public interface IDataflowPipeBulkInserter<TMessage, TResult>
+    {
+        #region Properties
+        Action<BulkInsertContextContext<TResult>> OnInsertCallBack { get; set; }
+
+        Func<IEnumerable<TMessage>, Task<IEnumerable<TResult>>> InsertHandler { get; set; }
+        #endregion
+
+        #region Bulk Insert Method
+        Task<IEnumerable<TResult>> BulkInsertAsync(IEnumerable<TMessage> messages, CancellationToken cancellationToken = default);
+        #endregion
+    }
+    #endregion
+
+    #region INoResultBulkInserter
+    /// <summary>
+    /// Bulk Inserter Interface
+    /// </summary>
+    public interface IDataflowNoResultHandler<TMessage>
+    {
+        #region Properties
+        Func<IEnumerable<TMessage>, Task> Handle { get; set; }
+
+        Action<BulkInsertContextContext> OnHandleCallBack { get; set; }
+        #endregion
+
+        #region Method
+        ValueTask HandleAsync(IEnumerable<TMessage> messages, CancellationToken cancellationToken = default);
+        #endregion
+    }
+    #endregion
+
+    #region Pipe IBulkInserter
+    /// <summary>
+    /// Bulk Inserter Interface
+    /// </summary>
+    public interface IPipeBulkInserter<TMessage, TResult> : IDisposable
+    {
+        #region Properties
+        Func<IEnumerable<TMessage>, Task<IEnumerable<TResult>>> InsertHandler { get; set; }
+
+        Action<BulkInsertContextContext<TResult>> OnInsertCallBack { get; set; }
+        #endregion
+
+        #region Bulk Insert Method
+        Task<TResult[]> BulkInsertAsync(IEnumerable<TMessage> messages, CancellationToken cancellationToken = default);
+
+        // Task BulkInserterAsync<TMessage>(IEnumerable<TMessage> messages, Func<IEnumerable<TMessage>> insertAction, CancellationToken cancellationToken = default);
+        #endregion
+    }
+    #endregion
+}
